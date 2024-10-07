fdd6481600ba9de6eea0a3e503a3ddb583a0f5dd
Jackie Chen <jackiechen@Jackies-MBP.lan>
2024-10-06 21:58:48 -0400

    new changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let user= prompt("Enter Name");
+// Function to handle the "Submit" button click
+function handleSubmitButtonClick() {
+    // code will go here
+}
+
+// Adding the event listener to the Submit button
+document.querySelector('.btn-4').addEventListener('click', handleSubmitButtonClick);
+
+
+
+ 
+
